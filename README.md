809597252b744daf49dd946eb7a97af92a857c3e
Da0hn <gabrielhondacba@gmail.com>
2024-06-15 14:58:44 -0400

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Java 21 Virtual Threads and Structured Concurrency
+
---
+++ src/main/java/dev/da0hn/structured/concurrency/Main.java
@@ -0,0 +1,9 @@
+package dev.da0hn.structured.concurrency;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+
+}
