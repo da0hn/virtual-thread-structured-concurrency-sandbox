3fba5eac79d1380d91103109979366185e5aebd6
Gabriel Honda <gabrielhondacba@gmail.com>
2024-10-04 10:59:01 -0400

    feat: adiciona exercicio de `Virtual Threads` + `CompletableFuture`

--- http-play/src/main/java/dev/da0hn/structured/concurrency/UserRequestHandler.java
+++ http-play/src/main/java/dev/da0hn/structured/concurrency/UserRequestHandler.java
@@ -6,6 +6,7 @@ import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Set;
 import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
@@ -13,10 +14,29 @@ public class UserRequestHandler implements Callable<String> {
 
   @Override
   public String call() throws Exception {
-    return parallelCall();
+    return callWithCompletableFuture();
   }
 
-  private String parallelCall() {
+  private String callWithCompletableFuture() {
+    try (var executorService = Executors.newVirtualThreadPerTaskExecutor()) {
+      final var start = Instant.now();
+
+      final var result = CompletableFuture.supplyAsync(this::databaseCall, executorService)
+        .thenCombineAsync(
+          CompletableFuture.supplyAsync(this::restApiCall, executorService),
+          (db, api) -> String.format("[%s, %s]", db, api)
+        ).join();
+
+      final var end = Instant.now();
+      System.out.println(result + " " + ChronoUnit.MILLIS.between(start, end) + "ms");
+      return result;
+    }
+    catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private String callWithFuture() {
     try (var executorService = Executors.newVirtualThreadPerTaskExecutor()) {
       final var start = Instant.now();
       final var result = executorService.invokeAll(Set.of(this::databaseCall, this::restApiCall)).stream()
@@ -35,7 +55,7 @@ public class UserRequestHandler implements Callable<String> {
     }
   }
 
-  private String sequentialCall() throws IOException, URISyntaxException {
+  private String sequentialCall() {
     final var start = Instant.now();
     final var result1 = this.databaseCall();
     final var result2 = this.restApiCall();
@@ -50,16 +70,26 @@ public class UserRequestHandler implements Callable<String> {
     return result;
   }
 
-  private String databaseCall() throws IOException, URISyntaxException {
-    System.out.println("Database: " + Thread.currentThread());
-    final var networkCaller = new NetworkCaller("db-call");
-    return networkCaller.makeCall(2);
+  private String databaseCall() {
+    try {
+      System.out.println("Database: " + Thread.currentThread());
+      final var networkCaller = new NetworkCaller("db-call");
+      return networkCaller.makeCall(2);
+    }
+    catch (IOException | URISyntaxException e) {
+      return null;
+    }
   }
 
-  private String restApiCall() throws IOException, URISyntaxException {
-    System.out.println("API Rest: " + Thread.currentThread());
-    final var networkCaller = new NetworkCaller("api-call");
-    return networkCaller.makeCall(5);
+  private String restApiCall() {
+    try {
+      System.out.println("API Rest: " + Thread.currentThread());
+      final var networkCaller = new NetworkCaller("api-call");
+      return networkCaller.makeCall(5);
+    }
+    catch (IOException | URISyntaxException e) {
+      return null;
+    }
   }
 
 }
